e0152e276c45a19e2f403530322c27a7d47b6836
Mad Hallé <mad.halle@icloud.com>
2020-03-25 09:15:14 -0600

    Add product file and product test file, working & complete

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,26 @@
-class Prodct
-
+`
+class Product
+  attr_accessor :category, :name, :unit_price, :quantity
   def initialize(category, name, unit_price, quantity)
     @category = category
-    @name = name_parameter
-    @unit_price = 3.70
-    @quantity = quantity
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity.to_i
   end
 
   def total_price
     @unit_price * @quantity
+  end
+
+  def is_hoarded?
+    if @quantity < 100
+      false
+    else
+      true
+    end
+  end
+
+  def hoard
+   @quantity += 100
   end
 end
